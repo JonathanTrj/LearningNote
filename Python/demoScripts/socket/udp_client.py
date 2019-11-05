3069000231d5d515222d959653cd23ef7baf860f
tangrj <tangrj@idtu.cn>
2019-11-05 17:21:29 +0800

    server

--- Python/demoScripts/socket/udp_client.py
+++ Python/demoScripts/socket/udp_client.py
@@ -6,7 +6,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 for data in [b'Michael', b'Tracy', b'Sarah', b'exit']:
     # 发送数据:
-    s.sendto(data, ('127.0.0.1', 9999))
+    s.sendto(data, ('172.105.199.214', 9999))
     # 接收数据:
     print(s.recv(1024).decode('utf-8'))
 
